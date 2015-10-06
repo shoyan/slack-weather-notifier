dbfa708015a4669f8697f1e09f7fff309ba3f6bd
Shohei Yamasaki <s-yamasaki@pepabo.com>
2015-10-06 14:00:49 +0900

    Use attachments

--- slack-weather-notifier.rb
+++ slack-weather-notifier.rb
@@ -6,8 +6,17 @@ uri = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=400010'
 
 res     = JSON.load(open(uri).read)
 title   = res['title']
+text    = res['description']['text']
 link    = res['link']
 weather = res['forecasts'].first
 
+attachments = [{
+  title: title,
+  title_link: link,
+  text: text,
+  image_url: weather['image']['url'],
+  color: "#7CD197"
+}]
+
 slack = Slack::Incoming::Webhooks.new ENV['WEBHOOK_URL']
-slack.post "<#{link}|#{weather['date']}の#{title}>は「#{weather['telop']}」です。"
+slack.post "#{weather['date']}の#{title}は「#{weather['telop']}」です。", attachments: attachments
